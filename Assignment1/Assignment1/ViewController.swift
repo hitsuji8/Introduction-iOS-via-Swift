fec0e1de6b65a5a22f5fd73c26cf39a751994ddd
Shota Aizawa <someone@nowhere.com>
2015-02-06 20:31:48 -0500

    Code button1 function
    Added method to increase one number

--- Assignment1/Assignment1/ViewController.swift
+++ Assignment1/Assignment1/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var score:Int = 0
+    
+    @IBAction func Clickone(sender: UIButton) {
+        score++
+        label.text = "Total: \(score)"
+    
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
